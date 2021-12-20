900a9b5ff5aa43c57da91c15215596c557f8ebed
ygev <1816215+ygev@users.noreply.github.com>
2021-12-19 17:09:40 -0800

    Implement border collision.

--- js/sketch.js
+++ js/sketch.js
@@ -3,24 +3,43 @@ var gif
 var x = 0;
 var y = 0;
 
-var imgWidth = 204;
-var imgHeight = 224;
+var xspeed;
+var yspeed;
 
-var speed = 10;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     background(11, 142, 128);
+    frameRate(20)
     gif = loadImage('img/gif-with-window.gif');
+    
+    x = random(width);
+    y = random(height);
+    xspeed = 40;
+    yspeed = 40;
+    
   }
   
   function draw() {
-    image(gif, x, 0);
-    if (( x + imgWidth) > width) {
-      speed = -10;
+
+    image(gif, x, y);
+    x = x + xspeed;
+    y = y + yspeed;
+  
+    if (x + gif.width >= width) {
+      xspeed = -xspeed;
+      x = width - gif.width;
+    } else if (x <= 0) {
+      xspeed = -xspeed;
+      x = 0;
     }
   
-    x = x + speed;
+    if (y + gif.height >= height) {
+      yspeed = -yspeed;
+      y = height - gif.height;
+    } else if (y <= 0) {
+      yspeed = -yspeed;
+      y = 0;
+    }
   }
-
   // https://editor.p5js.org/cmorgantywls/sketches/HkdbRQnOG
