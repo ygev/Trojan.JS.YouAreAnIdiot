6fb85b73c18e4d326923b34eba267ea131a70085
ygev <1816215+ygev@users.noreply.github.com>
2022-01-11 22:03:52 -0800

    Added the song.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # you-are-an-idiot
 ha ha ha ha ha ha ha
 
-  <img src="img/gif-with-window.gif" alt="hahahahha">
+  <img src="assets/gif-with-window.gif" alt="hahahahha">
--- js/sketch.js
+++ js/sketch.js
@@ -1,4 +1,5 @@
 var gif 
+var song
 
 var x = 0;
 var y = 0;
@@ -6,23 +7,57 @@ var y = 0;
 var xspeed;
 var yspeed;
 
+function preload() {
+  soundFormats('mp3', 'ogg');
+  song = loadSound('assets/idiot');
+  
+  gif = loadImage('assets/gif-with-window.gif');
+}
 
 function setup() {
     let mycanvas = createCanvas(windowWidth, windowHeight);
     mycanvas.parent('canvas');
-    // frameRate(20)
-    gif = loadImage('img/gif-with-window.gif');
     
+    // Window Moving Around
     x = random(width);
     y = random(height);
     xspeed = 5;
     yspeed = 5;
 
+    // Selection Logic
+    var iconImg = document.getElementsByClassName('icon-img')[0];
+    var iconTxt = document.getElementsByClassName('icon-txt')[0];
+    var icon = document.getElementsByClassName('icon')[0];
+    document.addEventListener('click', function(event) {
+      var isClickInside = icon.contains(event.target);
+
+      if (!isClickInside) {
+        iconTxt.classList.remove("icon-txt_selected");
+      }
+      else {
+        iconTxt.classList.add("icon-txt_selected");
+      }
+    });
+
+     // Double Click Logic
+     var leCanvas = document.getElementById('canvas');
+
+     leCanvas.style.display = "none"; 
+     document.addEventListener('dblclick', function(event) {
+      var isDoubleClickInside = icon.contains(event.target);
+      if (isDoubleClickInside){
+        leCanvas.style.display = "block"; 
+        song.loop();
+        iconTxt.classList.remove("icon-txt_selected");
+      }
+     });
+
   }
   
   function draw() {
     clear()
-    // background(11, 142, 128);
+
+  // Animation of the image
     image(gif, x, y);
     x = x + xspeed;
     y = y + yspeed;
@@ -42,6 +77,8 @@ function setup() {
       yspeed = -yspeed;
       y = 0;
     }
+
+    // If you click, start another instance of the song.
   }
 
   // https://editor.p5js.org/cmorgantywls/sketches/HkdbRQnOG
